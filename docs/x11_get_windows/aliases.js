a316b1718834e38454e398ec3bfd4e52d5dbd53d
Hiruna Jayamanne <hirunav2@gmail.com>
2018-10-15 00:21:58 +1300

    Updated docs, removed dependencies

--- docs/x11_get_windows/aliases.js
+++ docs/x11_get_windows/aliases.js
@@ -1,5 +1,2 @@
 var ALIASES = {};
-ALIASES['libc'] = {};
-ALIASES['x11'] = {};
-ALIASES['x11_get_windows'] = {};
 ALIASES['x11_get_windows'] = {};
--- docs/x11_get_windows/x11_get_windows/util/sidebar-items.js
+++ docs/x11_get_windows/x11_get_windows/util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_window_property","An export of [XGetWindowProperty]. Make sure to [xlib::x11::XFree] the pointer, when you're done with it."]],"struct":[["GetWindowPropertyResponse","A response to [get_window_property]."]]});
+initSidebarItems({"fn":[["get_window_property","An export of [XGetWindowProperty]. Make sure to [x11::xlib::XFree] the pointer, when you're done with it."]],"struct":[["GetWindowPropertyResponse","A response to [get_window_property]."]]});
