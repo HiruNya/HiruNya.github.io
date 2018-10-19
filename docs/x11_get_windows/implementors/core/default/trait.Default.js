d7ae6e2407734f0033d5bb98e9272cd44243c315
Hiruna Jayamanne <hirunav2@gmail.com>
2018-10-19 22:24:07 +1300

    Updated x11_get_windows docs

---
+++ docs/x11_get_windows/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["x11_get_windows"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"x11_get_windows/util/struct.GetWindowPropertyResponse.html\" title=\"struct x11_get_windows::util::GetWindowPropertyResponse\">GetWindowPropertyResponse</a>",synthetic:false,types:["x11_get_windows::util::get_window_property::GetWindowPropertyResponse"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
